1ae6168d10507fed60a423a84be0ae208d810014
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-28 15:59:33 +0900

    ✨ポケモンのデータを取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import { useEffect } from "react";
+
+import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
-  return (
-    <>
-      
-    </>
-  )
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+    };
+    fetchPokemonData();
+  }, []);
+
+  return <></>;
 }
 
-export default App
+export default App;
---
+++ src/utils/pokemon.js
@@ -0,0 +1,5 @@
+export const getAllPokemon = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+};
